3f747a5856bc8f90ef9bc5be1e814aad537ac9a3
Adriana Shulman <adriana33@mac.com>
2018-09-13 19:38:12 -0400

    updating files

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.PORT || "mongodb://localhost/nyt"
 
 // Initialize Express
 var app = express();
@@ -29,7 +30,8 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/nyt");
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URI);
 
 // Routes
 app.get("/scrape", function (req, res) {
