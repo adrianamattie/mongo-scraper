49419a076d8442957e7471b91061d7215daa39ab
Adriana Shulman <adriana33@mac.com>
2018-09-13 19:55:32 -0400

    updating files

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@
 
 
 // Grab the articles as a json
-$.getJSON("/articles", function (data) {
+$.get("/articles", function (data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
         // Display the apropos information on the page
